9751fe4c0d4696735d9b6e300c807ce09f991b8f
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-28 02:02:04 +0600

    load data in localstorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -12,12 +13,19 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+    }
+  }, []);
   const handleAddToCart = (product) => {
     console.log(product);
     //cart.push(product)
 
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
