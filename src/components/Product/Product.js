ba0dc4259aa063d51239a7309d8f5d3ab67fbd88
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-27 21:45:12 +0600

    fontawesome in react

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,3 +1,5 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css';
 
@@ -19,7 +21,8 @@ const Product = ({ product, handleAddToCart }) => {
         </p>
       </div>
       <button onClick={() => handleAddToCart(product)} className="btn-cart">
-        <p>Add to Cart</p>
+        <p className='btn-text'>Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
       </button>
     </div>
   );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import './Shop.css';
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart,setCart] = useState([]);
   useEffect(() => {
     fetch('products.json')
       .then((res) => res.json())
@@ -12,6 +13,11 @@ const Shop = () => {
 
   const handleAddToCart = (product) => {
     console.log(product);
+    //cart.push(product)
+
+    const newCart = [...cart,product];
+    setCart(newCart);
+
   };
 
   return (
@@ -27,6 +33,7 @@ const Shop = () => {
       </div>
       <div className="cart-container">
         <h4>Order Summary</h4>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );
