c08d4ce8ba71fae12569d137db1e1c350b0c3409
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-21 12:14:20 +0600

    context api setup done

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,14 @@
+// in UserContext.js
+import React, { createContext } from 'react';
+
+const AuthContext = createContext();
+const UserContext = ({children}) => {
+    const authInfo = {}
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
