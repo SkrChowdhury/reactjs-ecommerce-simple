89731308d7a173811cbf09a0c323521069dca3c3
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-21 14:04:11 +0600

    sign in and sign out using firebase implementation done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/UserContext';
 
 const About = () => {
-    return (
-        <div>
-            <h2>Secret About Us</h2>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2>Secret About Us</h2>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './Login.css';
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div className="form-container">
       <h2 className="form-title"> Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="" required />
+          <input type="email" name="email" required />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="" required />
+          <input type="password" name="password" required />
         </div>
-        <input className='btn-submit' type="submit" value="Login" />
+        <input className="btn-submit" type="submit" value="Login" />
       </form>
-      <p>New here? <Link to='/signup'>Create a new account</Link></p>
+      <p>
+        New here? <Link to="/signup">Create a new account</Link>
+      </p>
     </div>
   );
 };
